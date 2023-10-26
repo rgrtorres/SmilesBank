764f77a13891640e038c1dbad9e1ccb66d1212a3
Renan Torres (MSC Brasil Ltda) <renan.torres@msc.com>
2023-10-26 16:43:38 -0300

    First Commit

---
+++ SmileBank/Controllers/ExtractController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmileBank.Interfaces;
+
+namespace SmileBank.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+
+    public class ExtractController : Controller
+    {
+        public static List<IExtract> Extract = new()
+        {
+            new ("Salário", 2857.00, false, "Válido"),
+            new ("Super Mercado Guassu", 128.32, false, "Válido"),
+        };
+
+        [HttpGet("ListExtract")]
+        public IActionResult Index()
+        {
+            return Ok(Extract);
+        }
+
+        [HttpPost("InsertExtract")]
+        public IActionResult Insert(IExtract extract)
+        {
+            Extract.Add(extract);
+            return Ok(Extract);
+        }
+    }
+}
---
+++ SmileBank/Interfaces/IExtract.cs
@@ -0,0 +1,22 @@
+﻿namespace SmileBank.Interfaces
+{
+    public class IExtract
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public bool Type { get; set; }
+        public string Status { get; set; }
+
+        public IExtract(string description, double amount, bool type, string status)
+        {
+            Id = Guid.NewGuid();
+            Description = description;
+            Date = DateTime.Now;
+            Amount = amount;
+            Type = type;
+            Status = status;
+        }
+    }
+}
